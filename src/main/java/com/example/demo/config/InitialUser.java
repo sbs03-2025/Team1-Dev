3bbd80a32f1fc9ba0522b747e22b31071fd1ede7
ishihara <ishihara.ryoga@sbsnt.co.jp>
2025-06-24 17:14:42 +0900

    初期設定追加

--- src/main/java/com/example/demo/config/InitialUser.java
+++ src/main/java/com/example/demo/config/InitialUser.java
@@ -25,6 +25,7 @@ public class InitialUser {
 		return args -> {
 			System.out.println("ユーザー初期設定");
 			
+			// 部署の初期設定.
 			Department newDepHumanResrc = departmentRepository.findByName("人事部").orElseGet(() -> {
 				Department dep = new Department();
 				dep.setName("人事部");
@@ -49,6 +50,7 @@ public class InitialUser {
 				return departmentRepository.save(dep);
 			});
 			
+			// ユーザーの初期設定.
 			User testUser1 = userRepository.findByName("InitUser").orElseGet(() -> {
 				User user = new User();
 				user.setName("InitUser");
@@ -62,6 +64,7 @@ public class InitialUser {
 				System.out.println("InitUserを登録しました。");
 				return userRepository.save(user);
 			});
+			
 			User testUser2 = userRepository.findByName("InitAdminUser").orElseGet(() -> {
 				User user = new User();
 				user.setName("InitAdminUser");
@@ -104,6 +107,63 @@ public class InitialUser {
 				return userRepository.save(user);
 			});
 			
+			User ishihara = userRepository.findByName("ishihara").orElseGet(() -> {
+				User user = new User();
+				user.setName("ishihara");
+				user.setEmail("ishihara@mail.com");
+				user.setPasswordHash(encoder.encode("ishihara"));
+				user.setRole("ROLE_USER");
+				user.setMyDepartment(Collections.singletonList(newDepIt));
+				user.setHobby("散歩");
+				user.setBio("你好");
+				user.setJoinedAt(LocalDateTime.of(2025,4,1,0,0));
+				System.out.println("ishiharaを登録しました。");
+				return userRepository.save(user);
+			});
+			
+			User furuta = userRepository.findByName("furuta").orElseGet(() -> {
+				User user = new User();
+				user.setName("furuta");
+				user.setEmail("furuta@mail.com");
+				user.setPasswordHash(encoder.encode("furuta"));
+				user.setRole("ROLE_USER");
+				user.setMyDepartment(Collections.singletonList(newDepIt));
+				user.setHobby("食べ歩き");
+				user.setBio("Hello!");
+				user.setJoinedAt(LocalDateTime.of(2025,4,1,0,0));
+				System.out.println("furutaを登録しました。");
+				return userRepository.save(user);
+			});
+			
+			User takiguchi = userRepository.findByName("takiguchi").orElseGet(() -> {
+				User user = new User();
+				user.setName("takiguchi");
+				user.setEmail("takiguchi@mail.com");
+				user.setPasswordHash(encoder.encode("takiguchi"));
+				user.setRole("ROLE_USER");
+				user.setMyDepartment(Collections.singletonList(newDepIt));
+				user.setHobby("読書");
+				user.setBio("こんにちは");
+				user.setJoinedAt(LocalDateTime.of(2025,4,1,0,0));
+				System.out.println("takiguchiを登録しました。");
+				return userRepository.save(user);
+			});
+			
+			User arai = userRepository.findByName("arai").orElseGet(() -> {
+				User user = new User();
+				user.setName("arai");
+				user.setEmail("arai@mail.com");
+				user.setPasswordHash(encoder.encode("arai"));
+				user.setRole("ROLE_ADMIN");
+				user.setMyDepartment(Collections.singletonList(newDepIt));
+				user.setHobby("散歩");
+				user.setBio("こんにちは");
+				user.setJoinedAt(LocalDateTime.of(2025,4,1,0,0));
+				System.out.println("araiを登録しました。");
+				return userRepository.save(user);
+			});
+			
+			// ユーザーのグループ.
 			List<User> userList12 = new ArrayList<>();
 			userList12.add(testUser1);
 			userList12.add(testUser2);
@@ -115,7 +175,14 @@ public class InitialUser {
 			userList12.add(testUser2);
 			userList12.add(testUser4);
 			
-			if(scheduleRepository.findByTitle("test").isEmpty()) {
+			List<User> userListAll = new ArrayList<>();
+			userListAll.add(ishihara);
+			userListAll.add(furuta);
+			userListAll.add(takiguchi);
+			userListAll.add(arai);
+			
+			// 予定の初期設定.
+			if(scheduleRepository.findByTitle("test1").isEmpty()) {
 				Schedule schedule = new Schedule();
 				schedule.setTitle("test1");
 				schedule.setDescription("description");
@@ -127,7 +194,7 @@ public class InitialUser {
 				scheduleRepository.save(schedule);
 			}
 			
-			if(scheduleRepository.findByTitle("test").isEmpty()) {
+			if(scheduleRepository.findByTitle("test2").isEmpty()) {
 				Schedule schedule = new Schedule();
 				schedule.setTitle("test2");
 				schedule.setDescription("description");
@@ -139,7 +206,7 @@ public class InitialUser {
 				scheduleRepository.save(schedule);
 			}
 			
-			if(scheduleRepository.findByTitle("test").isEmpty()) {
+			if(scheduleRepository.findByTitle("test3").isEmpty()) {
 				Schedule schedule = new Schedule();
 				schedule.setTitle("test3");
 				schedule.setDescription("description");
@@ -147,6 +214,18 @@ public class InitialUser {
 				schedule.setEndDateTime(LocalDateTime.of(2025,5,1,0,0));
 				schedule.setParticipants(userList24);
 				schedule.setCreatedUser(testUser4);
+				
+				scheduleRepository.save(schedule);
+			}
+			
+			if(scheduleRepository.findByTitle("test4").isEmpty()) {
+				Schedule schedule = new Schedule();
+				schedule.setTitle("AllMember");
+				schedule.setDescription("Meeting");
+				schedule.setStartDateTime(LocalDateTime.of(2025,6,1,0,0));
+				schedule.setEndDateTime(LocalDateTime.of(2025,7,1,0,0));
+				schedule.setParticipants(userListAll);
+				schedule.setCreatedUser(ishihara);
 				
 				scheduleRepository.save(schedule);
 			}
